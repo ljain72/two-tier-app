692f7dc811bda55441c847db94d14593e446bbea
Labdhi Jain <labdhi96jain@gmail.com>
2026-01-22 20:14:06 +0100

    edited provider

--- provider.tf
+++ provider.tf
@@ -1,10 +1,21 @@
 provider "helm" {
-  kubernetes = {
+  kubernetes  {
     host                   = data.aws_eks_cluster.this.endpoint
     cluster_ca_certificate = base64decode(
       data.aws_eks_cluster.this.certificate_authority[0].data
     )
     token = data.aws_eks_cluster_auth.this.token
+  }
+}
+
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "= 2.5.1"
+    }
   }
 }
 
