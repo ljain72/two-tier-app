c15da76c72c344928b921f9245ba88491c6e54ba
Labdhi Jain <labdhi96jain@gmail.com>
2026-01-22 20:07:05 +0100

    Initial commit

---
+++ provider.tf
@@ -0,0 +1,17 @@
+provider "helm" {
+  kubernetes = {
+    host                   = data.aws_eks_cluster.this.endpoint
+    cluster_ca_certificate = base64decode(
+      data.aws_eks_cluster.this.certificate_authority[0].data
+    )
+    token = data.aws_eks_cluster_auth.this.token
+  }
+}
+
+data "aws_eks_cluster" "this" {
+  name = "test"
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = "test"
+}
